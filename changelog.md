0280c2996012cf39e309e343a5ac20e5b3a20737
Shaikzaheer174 <shaikzaheer174@gmail.com>
2025-01-05 23:10:21 +0530

    Added let, const and var keywords

--- changelog.md
+++ changelog.md
@@ -1,1 +1,2 @@
 1.  linking a js file.
+2.  Added let, const and var keywords.
--- fundamentals 1/main.js
+++ fundamentals 1/main.js
@@ -1,1 +1,18 @@
 alert("Hello world! from external script");
+
+// if you want to reassign the values you can use let keyword.
+let age = 20;
+age = 25;
+
+// const birthDate;  //'const' declarations must be initialized.
+const birthYear = 1945;
+
+var job = "programmer"; 
+job = "developer"; 
+
+console.log(a); // undefined (hoisted)
+var a = 5;
+
+//console.log(b); // Error: Cannot access 'b' before initialization
+let b = 10;
+
